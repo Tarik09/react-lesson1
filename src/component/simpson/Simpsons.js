08fffec846be646d8376a25a9df45995d887832d
Tarik09 <nudga.t4@gmail.com>
2021-08-11 21:12:52 +0300

    commit

--- src/component/simpson/Simpsons.js
+++ src/component/simpson/Simpsons.js
@@ -1,10 +1,9 @@
 export default function Simpsons({name,surname,age,info,photo}) {
-
-    return (
-      <div className={'simpsons'}>
+  return (
+    <div className={'simpsons'}>
         <h2>{name} {surname} - {age}</h2>
         <img src={photo} alt=""/>
         <p>{info}</p>
-      </div>
+    </div>
   );
 }
